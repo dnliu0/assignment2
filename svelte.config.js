b9585beb3c6374cad2e9f2fcd9c9957ae4642753
Danni Liu <danniliu@Dannis-MacBook-Pro.local>
2024-10-30 20:37:19 -0500

    test

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import adapter from '@sveltejs/adapter-netlify';
 
 export default {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      edge: true,
+      split: false
+    })
   },
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
